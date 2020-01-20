08dfa5a088e12b7d6226cd903c4300ec883b0713
Ivan <i_cuenca@aol.com>
2020-01-20 15:34:50 -0500

    Updates

---
+++ precache-manifest.14186b899f944af63073c9033830fe1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a33abaea221d40fe94408e13148e8d",
+    "url": "/api_random_user/index.html"
+  },
+  {
+    "revision": "5aad17d37ac70177fa52",
+    "url": "/api_random_user/static/css/main.f3f4d41c.chunk.css"
+  },
+  {
+    "revision": "e231399506cd104a52ba",
+    "url": "/api_random_user/static/js/2.ebc9cbea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/api_random_user/static/js/2.ebc9cbea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5aad17d37ac70177fa52",
+    "url": "/api_random_user/static/js/main.cc2c6928.chunk.js"
+  },
+  {
+    "revision": "d370183a9711a6b242b4",
+    "url": "/api_random_user/static/js/runtime-main.daea8ea1.js"
+  }
+]);
